4cd1c5eaf71d2837bef960c66310ab2bf8a74d14
tim-moody <tim@timmoody.com>
2022-01-10 13:14:18 -0500

    common and cache loader

---
+++ common.py
@@ -0,0 +1,27 @@
+# common functions
+import sys
+import json
+
+# taken from sp_lib
+def read_json_file(file_path):
+    try:
+        with open(file_path, 'r') as json_file:
+            readstr = json_file.read()
+            json_dict = json.loads(readstr)
+        return json_dict
+    except OSError as e:
+        print('Unable to read url json file', e)
+        raise
+
+def write_json_file(src_dict, target_file, sort_keys=False):
+    try:
+        with open(target_file, 'w', encoding='utf8') as json_file:
+            json.dump(src_dict, json_file, ensure_ascii=False, indent=2, sort_keys=sort_keys)
+            json_file.write("\n")  # Add newline cause Py JSON does not
+    except OSError as e:
+        raise
+
+def write_list(data, file):
+    with open(file, 'w') as f:
+        for d in data:
+            f.write(d + '\n')
